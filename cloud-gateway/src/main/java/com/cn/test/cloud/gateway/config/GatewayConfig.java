bbdc76a69fc8c8db10fd86cb6aa882ceef0d0257
chennan <327919006@qq.com>
2019-08-03 18:45:44 +0800

    整合SpringCloudGateway

---
+++ cloud-gateway/src/main/java/com/cn/test/cloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.cn.test.cloud.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Chen Nan
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/order/**")
+                        .filters(f -> {
+                            f.addResponseHeader("myHeader", "test01");
+                            f.addRequestHeader("myHeader", "test02");
+                            return f;
+                        }
+
+                        )
+                        .uri("http://localhost:10091")
+                )
+                .build();
+    }
+}
---
+++ cloud-gateway/src/main/java/com/cn/test/cloud/gateway/filter/AuthFilter.java
@@ -0,0 +1,59 @@
+package com.cn.test.cloud.gateway.filter;
+
+import cn.hutool.json.JSONUtil;
+import com.cn.test.cloud.common.model.dto.RspBase;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Chen Nan
+ */
+@Component
+@Order(0)
+public class AuthFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        String token = request.getHeaders().getFirst("token");
+        if (validToken(token)) {
+            return chain.filter(exchange);
+        }
+
+        ServerHttpResponse response = exchange.getResponse();
+
+        RspBase rspBase = new RspBase();
+        rspBase.setMsg("请先登录");
+
+        DataBuffer dataBuffer = response.bufferFactory().wrap(JSONUtil.toJsonStr(rspBase).getBytes());
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().add("Content-Type", MediaType.APPLICATION_PROBLEM_JSON_UTF8_VALUE);
+        return response.writeWith(Mono.just(dataBuffer));
+    }
+
+    /**
+     * 校验token是否有效
+     *
+     * @param accessToken accessToken
+     * @return true token有效  false token无效
+     */
+    private boolean validToken(String accessToken) {
+        if (StringUtils.isBlank(accessToken)) {
+            return false;
+        }
+
+        // 根据业务规则校验token是否有效
+        return accessToken.contains("1");
+    }
+}
