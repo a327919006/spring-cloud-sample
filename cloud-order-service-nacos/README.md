e024e482b858531c84ab67b441f606e16f18abd6
chennan <327919006@qq.com>
2019-08-04 11:39:34 +0800

    整合nacos配置中心

---
+++ cloud-order-service-nacos/README.md
@@ -0,0 +1,34 @@
+## cloud-order-service-nacos
+
+#### 模块说明
+订单服务，使用nacos
+
+#### Maven模块描述
+
+| 端口 | 名称 | 描述 |
+| --- | --- | --- |
+| ----- | cloud-common | 系统工具类、服务接口、实体类封装 |
+| 8070  | cloud-config-server | 配置中心 |
+| ----- | cloud-dal | mybatisMapper，sql |
+| 8761  | cloud-eureka | eureka注册中心 |
+| 8763  | cloud-eureka-ha | eureka注册中心集群 |
+| 8060,8720  | cloud-gateway | spring-cloud-gateway网关，注册中心为nacos，整合sentinel |
+| 8040  | cloud-gateway-zuul | zuul网关 |
+| 8030  | cloud-hystrix-dashboard | hystrix可视化 |
+| 8031  | cloud-hystrix-turbine | hystrix多模块监控 |
+| 8050  | cloud-sidecar | 多语言支持，springCloud包裹其他语言的接口，如php等 |
+| 10090 | cloud-user-service | 用户服务，注册中心为eureka |
+| 10090 | cloud-user-service-consul | 用户服务，注册中心为consul |
+| 10090 | cloud-user-service-nacos | 用户服务，注册中心为nacos |
+| 10090 | cloud-user-service-zk | 用户服务，注册中心为zookeeper |
+| 10091 | cloud-order-service | 订单服务，注册中心为eureka |
+| 10091 | cloud-order-service-consul | 订单服务，注册中心为consul |
+| 10091,8719 | cloud-order-service-nacos | 订单服务，注册中心为nacos，整合sentinel |
+| 10091 | cloud-order-service-zk | 订单服务，注册中心为zookeeper |
+| 9411  | cloud-zipkin | 链路跟踪 |
+
+#### 启动步骤
+- 启动eureka注册中心
+- 启动配置中心
+- 启动user服务
+- 启动order服务
--- cloud-order-service-nacos/src/main/java/com/cn/test/cloud/user/service/nacos/controller/OrderController.java
+++ cloud-order-service-nacos/src/main/java/com/cn/test/cloud/user/service/nacos/controller/OrderController.java
@@ -4,12 +4,11 @@ import com.cn.test.cloud.common.model.dto.RspBase;
 import com.cn.test.cloud.user.service.nacos.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -33,9 +32,15 @@ public class OrderController {
     @Autowired
     private UserService userService;
 
+    @Value("${user.age}")
+    private String age;
+
+    @Autowired
+    private ConfigurableApplicationContext config;
+
     @GetMapping("/{id}")
-    public Object get(@PathVariable String id) {
-        log.info("【订单】开始获取");
+    public RspBase get(@PathVariable String id, @RequestHeader(required = false) String myHeader) {
+        log.info("【订单】开始获取,myHeader={}", myHeader);
         ServiceInstance serviceInstance = loadBalancerClient.choose("cloud-user-service-nacos");
         String url = String.format("http://%s:%s/user/%s", serviceInstance.getHost(), serviceInstance.getPort(), id);
         RspBase result = restTemplate.getForObject(url, RspBase.class);
@@ -44,10 +49,25 @@ public class OrderController {
     }
 
     @GetMapping("/feign/{id}")
-    public Object feignGet(@PathVariable String id) {
+    public RspBase feignGet(@PathVariable String id) {
         log.info("【订单】开始获取");
         RspBase rspBase = userService.get(id);
         log.info("【订单】获取成功");
         return rspBase;
+    }
+
+    @GetMapping("/config/{key}")
+    public String getConfig(@PathVariable String key) {
+        log.info("【配置】开始获取,key={}", key);
+        String name = config.getEnvironment().getProperty(key);
+        log.info("【配置】获取成功");
+        return name;
+    }
+
+    @GetMapping("/config/age")
+    public String getAge() {
+        log.info("【配置】开始获取age");
+        log.info("【配置】获取成功age");
+        return age;
     }
 }
